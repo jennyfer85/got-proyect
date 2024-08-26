6c2aeeecb69c61f6ca11976db70292bc6d5a6d42
jennyfer85 <jmtg05@gmail.com>
2024-08-26 11:46:17 +0200

    button delete

--- src/services.js
+++ src/services.js
@@ -2,6 +2,12 @@ const URL_API = "http://localhost:8000/character"
 
 //CREATE METHOD : (POST)
 async function createCharacter() {
+    const response = await fetch(URL_API, {
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+    })
 
 }
 // READ METHOD : (GET)
@@ -12,32 +18,32 @@ async function getAllCharacter() {
             "Content-Type": "application/json", // Indica que estamos trabajando con JSON
         },
     });
-    const data = await response.json() // convierto la respuesta a json
+    const data = await response.json() // convierto la respuesta a json. Pasar datos a formato json
     return data
 }
+
 const listTag = document.getElementById('charactersList')
 
 //PINTAr los datos de un get 
-async function printCharacter() {
-    const characters = await getAllCharacter() //
-    characters.map((character) => {
-        listTag.innerHTML += `
-            <li>
+async function printCharacters() {
+    const characters = await getAllCharacter() // llamo a la funcion getAllCharacter
+    listTag.innerHTML = ''
+        listTag.innerHTML += `<li>
             <p>${character.name}</p>
-            <p> ${character.house}</p>
-            <p> ${character.age}</p>
+            <p>${character.house}</p>
+            <p>${character.age}</p>
             <button onclick="deleteCharacter(${character.id})">delete</button>
-            </li>
-            
-        `
+            </li>`
     })
 }
-printCharacter()
+printCharacters()
 
 //UPDATE METHOD : (PUT)
 async function updateCharacter() {
 
 }
+
+
 //DELETE METHOD : (DELETE)
 async function deleteCharacter(id) {
     const response = await fetch(URL_API + `/${id}`, { //añadir el id para 
@@ -47,6 +53,9 @@ async function deleteCharacter(id) {
         },
     })
     const deletedCharacter = await response.json()
+    if (response.ok){
+        printCharacters()
+    }
     return deletedCharacter
 }
 
